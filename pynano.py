caa772f4ba0e94c30ca4447d6dbeb3fc472a57fa
mcwindy <40684304+mcwindy@users.noreply.github.com>
2022-06-11 06:04:14 +0800

    Update pynano.py

--- pynano.py
+++ pynano.py
@@ -8,126 +8,22 @@ tab_equivalent_length = 4
 cursor = [0, 0]
 modified = False
 show_save_menu = False
+show_search_menu = False
 terminal_size = os.get_terminal_size()
 encoding = 'utf-8'
-
-# snippets from stackoverflow, like fine art in the trash
-import errno
-import tempfile
-
-ERROR_INVALID_NAME = 123
+search_word = ''
+search_status = 0
+history = []
 
 
-def is_pathname_valid(pathname: str) -> bool:
-    '''
-    `True` if the passed pathname is a valid pathname for the current OS;
-    `False` otherwise.
-    '''
-    # If this pathname is either not a string or is but is empty, this pathname
-    # is invalid.
-    try:
-        if not isinstance(pathname, str) or not pathname:
-            return False
+def record_history(func):  # TODO[enhancemnet] record which operation and changes to save space
 
-        # Strip this pathname's Windows-specific drive specifier (e.g., `C:\`)
-        # if any. Since Windows prohibits path components from containing `:`
-        # characters, failing to strip this `:`-suffixed prefix would
-        # erroneously invalidate all valid absolute Windows pathnames.
-        _, pathname = os.path.splitdrive(pathname)
+    def wrapper(content, cursor):
+        global history
+        history.append((content[:], cursor[:]))
+        return func(content, cursor)
 
-        # Directory guaranteed to exist. If the current OS is Windows, this is
-        # the drive to which Windows was installed (e.g., the "%HOMEDRIVE%"
-        # environment variable); else, the typical root directory.
-        root_dirname = os.environ.get('HOMEDRIVE', 'C:') \
-            if sys.platform == 'win32' else os.path.sep
-        assert os.path.isdir(root_dirname)  # ...Murphy and her ironclad Law
-
-        # Append a path separator to this directory if needed.
-        root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
-
-        # Test whether each path component split from this pathname is valid or
-        # not, ignoring non-existent and non-readable path components.
-        for pathname_part in pathname.split(os.path.sep):
-            try:
-                os.lstat(root_dirname + pathname_part)
-            # If an OS-specific exception is raised, its error code
-            # indicates whether this pathname is valid or not. Unless this
-            # is the case, this exception implies an ignorable kernel or
-            # filesystem complaint (e.g., path not found or inaccessible).
-            #
-            # Only the following exceptions indicate invalid pathnames:
-            #
-            # * Instances of the Windows-specific "WindowsError" class
-            #   defining the "winerror" attribute whose value is
-            #   "ERROR_INVALID_NAME". Under Windows, "winerror" is more
-            #   fine-grained and hence useful than the generic "errno"
-            #   attribute. When a too-long pathname is passed, for example,
-            #   "errno" is "ENOENT" (i.e., no such file or directory) rather
-            #   than "ENAMETOOLONG" (i.e., file name too long).
-            # * Instances of the cross-platform "OSError" class defining the
-            #   generic "errno" attribute whose value is either:
-            #   * Under most POSIX-compatible OSes, "ENAMETOOLONG".
-            #   * Under some edge-case OSes (e.g., SunOS, *BSD), "ERANGE".
-            except OSError as exc:
-                if hasattr(exc, 'winerror'):
-                    if exc.winerror == ERROR_INVALID_NAME:
-                        return False
-                elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
-                    return False
-    # If a "TypeError" exception was raised, it almost certainly has the
-    # error message "embedded NUL character" indicating an invalid pathname.
-    except TypeError as exc:
-        return False
-    # If no exception was raised, all path components and hence this
-    # pathname itself are valid. (Praise be to the curmudgeonly python.)
-    else:
-        return True
-    # If any other exception was raised, this is an unrelated fatal issue
-    # (e.g., a bug). Permit this exception to unwind the call stack.
-    #
-    # Did we mention this should be shipped with Python already?
-
-
-def is_path_sibling_creatable(pathname: str) -> bool:
-    '''
-    `True` if the current user has sufficient permissions to create **siblings**
-    (i.e., arbitrary files in the parent directory) of the passed pathname;
-    `False` otherwise.
-    '''
-    # Parent directory of the passed path. If empty, we substitute the current
-    # working directory (CWD) instead.
-    dirname = os.path.dirname(pathname) or os.getcwd()
-
-    try:
-        # For safety, explicitly close and hence delete this temporary file
-        # immediately after creating it in the passed path's parent directory.
-        with tempfile.TemporaryFile(dir=dirname):
-            pass
-        return True
-    # While the exact type of exception raised by the above function depends on
-    # the current version of the Python interpreter, all such types subclass the
-    # following exception superclass.
-    except EnvironmentError:
-        return False
-
-
-def is_path_exists_or_creatable_portable(pathname: str) -> bool:
-    '''
-    `True` if the passed pathname is a valid pathname on the current OS _and_
-    either currently exists or is hypothetically creatable in a cross-platform
-    manner optimized for POSIX-unfriendly filesystems; `False` otherwise.
-
-    This function is guaranteed to _never_ raise exceptions.
-    '''
-    try:
-        # To prevent "os" module calls from raising undesirable exceptions on
-        # invalid pathnames, is_pathname_valid() is explicitly called first.
-        return is_pathname_valid(pathname) and (os.path.exists(pathname) or is_path_sibling_creatable(pathname))
-    # Report failure on non-fatal filesystem complaints (e.g., connection
-    # timeouts, permissions issues) implying this path to be inaccessible. All
-    # other exceptions are unrelated fatal issues and should not be caught here.
-    except OSError:
-        return False
+    return wrapper
 
 
 def save_file(filename, content):
@@ -137,7 +33,7 @@ def save_file(filename, content):
 
 
 def show_screen(content):
-    global show_save_menu, cursor
+    global show_save_menu, show_search_menu, cursor
     os.system('cls')
     print('\tpynano v0.1')
     print('-' * terminal_size.columns, end='')
@@ -147,7 +43,7 @@ def show_screen(content):
                 print('\033[4;30;47m{}\033[0m{}'.format(
                     *((' \n', '') if char == '\n' else (' ', ' ' * (tab_equivalent_length - 1)) if char == '\t' else (' ', '') if char == '\x00' else (char,
                                                                                                                                                        ''))),
-                      end='')
+                      end='')  # highlight bug
             else:
                 print(' ' * tab_equivalent_length if char == '\t' else ' ' if char == '\x00' else char, end='')
     print('\n~' * (terminal_size.lines - len(content) - 6 - debug * (1 + len(content))))
@@ -166,13 +62,25 @@ def show_screen(content):
                 save_file(filename, content)
                 my_exit()
             elif key == b'N' or key == b'n':
-                # TODO save file to tmp
+                # TODO[enhancement] save file to tmp
                 my_exit()
+    elif show_search_menu:
+        global search_word
+        print('Word to search: ' + search_word)
+        print('C-c to cancel')
+        if search_status == 1:
+            print('Found')
+        elif search_status == 2:
+            print('Start search from begining, not found')
+        elif search_status == 3:
+            print('Start search from end, found')
+
     else:
         print('C-x to save | C-c to exit')
     if debug:
         print(cursor)
         print(content)
+        print(history)
 
 
 def get_filename():
@@ -221,6 +129,7 @@ def cursor_to_end(content, cursor):
     return content, cursor
 
 
+@record_history
 def delete(content, cursor):
     global modified
     modified = True
@@ -252,6 +161,14 @@ def save(content, cursor):
         my_exit()
 
 
+def undo(content, cursor):
+    global history
+    if len(history) != 0:
+        return history.pop()
+    else:
+        return content, cursor
+
+
 def handle_control_characters(content, cursor):
 
     control_character_func_dict = {
@@ -269,6 +186,7 @@ def handle_control_characters(content, cursor):
         return control_character_func_dict[key](content, cursor)
 
 
+@record_history
 def handle_backspace(content, cursor):
     global modified
     modified = True
@@ -284,6 +202,7 @@ def handle_backspace(content, cursor):
     return content, cursor
 
 
+@record_history
 def handle_enter(content, cursor):
     global modified
     modified = True
@@ -293,6 +212,13 @@ def handle_enter(content, cursor):
     return content, cursor
 
 
+def handle_search(content, cursor):
+    global show_search_menu
+    show_search_menu = True
+    return content, cursor
+
+
+@record_history
 def handle_tabulator(content, cursor):
     global modified
     modified = True
@@ -301,6 +227,7 @@ def handle_tabulator(content, cursor):
     return content, cursor
 
 
+@record_history
 def handle_printable_character(content, cursor):
     global modified
     modified = True
@@ -309,29 +236,68 @@ def handle_printable_character(content, cursor):
     return content, cursor
 
 
+def search(content, cursor, word):
+    global search_status
+    search_status = 1  # 1: found 2:from beginning
+    if word in inline_next_content:
+        cursor[1] += inline_next_content.index(word) + 1
+    else:
+        search_status |= 2
+        for i in range(cursor[0] + 1, len(content)):
+            if word in content[i]:
+                cursor[0] = i
+                cursor[1] = content[i].index(word)
+                break
+        else:
+            for i in range(0, cursor[0]):
+                if word in content[i]:
+                    cursor[0] = i
+                    cursor[1] = content[i].index(word)
+                    break
+            else:
+                if word in current_line:
+                    cursor[1] = current_line.index(word)
+                else:
+                    search_status ^= 1
+                    print('word not found')
+
+
+def search_menu():
+    global show_search_menu
+    if key == b'\x03':
+        show_search_menu = False
+    elif key == b'\x08':
+        search_word = search_word[:-1]
+    elif key == b'\x0d':
+        search(content, cursor, search_word)
+        show_search_menu = False
+    elif 32 <= ord(key) < 127:
+        search_word += key.decode('utf-8')
+        print("search_word:", search_word)
+
+
 def load_file():
     if len(sys.argv) == 2:
         target_path = sys.argv[1]
         if os.path.exists(target_path):
             if os.path.isdir(target_path):
-                # TODO choose file
+                # TODO[enhancement] choose file
                 error_exit('This is a directory, not a file. Exiting.')
             elif os.path.isfile(target_path):
                 with open(target_path, 'r') as f:
-                    content = f.readlines() or ['']
-                    if content[-1][-1] == '\n':
-                        content.append('\x00')
-                    else:
+                    content = f.readlines()
+                    if content:
                         content[-1] += '\x00'
+                    else:
+                        content.append('\x00')
+
                     show_screen(content)
                 return target_path, content
         else:
-            if is_path_exists_or_creatable_portable(target_path):
-                open(target_path, 'w').close()  # TODO
-                content = ['\x00']
-                show_screen(content)
-            else:
-                error_exit('This path is invalid.')
+            open(target_path, 'w').close()
+            content = ['\x00']
+            show_screen(content)
+            return target_path, content
     else:
         error_exit('Usage: python3 pynano.py <file>')
 
@@ -341,17 +307,20 @@ func_dict = {
     b'\x08': handle_backspace,  # backspace
     b'\x09': handle_tabulator,  # tabulator
     b'\x0d': handle_enter,  # enter
+    b'\x17': handle_search,  # C-w
     b'\x18': save,  # C-x
+    b'\x1a': undo,
     b'\xe0': handle_control_characters,
     **{chr(c).encode(encoding): handle_printable_character
        for c in range(32, 127)}  # printable characters
 }
 
-# TODO copy paste cut shift-select undo
+# TODO copy paste cut shift-select
 if __name__ == '__main__':
     target_path, content = load_file()
 
     while True:
+        show_screen(content)
         key = getch()
         current_line = content[cursor[0]]
         previous_content = content[:cursor[0]]
@@ -360,7 +329,8 @@ if __name__ == '__main__':
         inline_next_content = current_line[cursor[1] + 1:]
         current_char = current_line[cursor[1]]
 
-        if key in func_dict:
-            content, cursor = func_dict[key](content, cursor)
-
-        show_screen(content)
+        if show_search_menu:
+            search_menu(key)
+        else:
+            if key in func_dict:
+                content, cursor = func_dict[key](content, cursor)
